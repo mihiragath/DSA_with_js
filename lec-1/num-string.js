68e70ae83efe94806b993fad7a07619f00c6da7a
mihiragath <mihiragath@users.noreply.github.com>
2025-07-25 22:09:52 +0530

    new

---
+++ lec-1/num-string.js
@@ -0,0 +1,22 @@
+a = 10;
+console.log(a);
+var a; //ans = 10
+//but using let or const it will not work we first initialise the variable then use it
+
+let b = 20;
+let c = 30;
+console.log("sum of no is:"+ b+c); //ans = sum of no is:2030
+//to get the sum we need to use brackets
+//nuber + integer = string concatenation
+console.log("sum of no is:" + (b + c)); //ans = sum of no is:50
+console.log(b+c+" is the sum of b and c"); //ans = 50 is the sum of b and 
+
+console.log("1"-1); //ans = 0
+console.log("1"+1); //ans = 11
+console.log("1"*0); //ans = 0
+console.log("1"/1); //ans = 1
+
+
+
+
+
