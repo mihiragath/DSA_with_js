3f3fe43398b0a4feaf3be43b3548ca98443e6566
mihiragath <mihiragath@users.noreply.github.com>
2025-07-30 14:09:30 +0530

    new

--- lec-4/advance_patter.js
+++ lec-4/advance_patter.js
@@ -26,3 +26,31 @@ for (let i = 1; i <= b; i++) {
   }
   process.stdout.write("\n");
 }
+
+let f = 5;
+for (let i = 1; i <= f; i++) {
+  for (let j = 1; j <= f - i; j++) {
+    process.stdout.write(" ");
+  }
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("*");
+  }
+  for (let j = 1; j < i; j++) {
+    process.stdout.write("*");
+  }
+  process.stdout.write("\n");
+}
+
+let n = 4;
+
+for (let i = 1; i <= n; i++) {
+  for (let s = 1; s <= n - i; s++) {
+    process.stdout.write(" ");
+  }
+
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("* ");
+  }
+
+  process.stdout.write("\n");
+}
--- lec-4/pattern.js
+++ lec-4/pattern.js
@@ -45,31 +45,3 @@ for (let i = 1; i <= e; i++) {
   }
   process.stdout.write("\n");
 }
-
-let f = 5;
-for (let i = 1; i <= f; i++) {
-  for (let j = 1; j <= f - i; j++) {
-    process.stdout.write(" ");
-  }
-  for (let j = 1; j <= i; j++) {
-    process.stdout.write("*");
-  }
-  for (let j = 1; j < i; j++) {
-    process.stdout.write("*");
-  }
-  process.stdout.write("\n");
-}
-
-let n = 4;
-
-for (let i = 1; i <= n; i++) {
-  for (let s = 1; s <= n - i; s++) {
-    process.stdout.write(" ");
-  }
-
-  for (let j = 1; j <= i; j++) {
-    process.stdout.write("* ");
-  }
-
-  process.stdout.write("\n");
-}
